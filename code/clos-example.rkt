8a568ef7ce1ea3f7618d2e0cc07c61b21436c74a
Manuela Beckert <sayuri-maus@web.de>
2017-05-07 17:30:03 +0200

    clos und object racket beispiel ueberarbeitet

--- code/clos-example.rkt
+++ code/clos-example.rkt
@@ -1,37 +1,53 @@
 #lang swindle
 
-(defclass Thing ())
+(defclass Thing ()
+  (name 
+    :accessor name
+    :initvalue "a Thing")
+  :printer #t)
+
+; (make Thing)
+
+(defgeneric (who-are-you? (t Thing)))
+(defmethod (who-are-you? (t Thing))
+  (string-append "I am " (name t) "!"))
+
+; (who-are-you? (make Thing))
 
 (defclass Element (Thing)
+  (name :initvalue "an Element")  ; first without
   (attr
     :accessor attr
     :initvalue 'water
     :initarg :attr)
-  (color
-    :accessor color
-    :initvalue 'blue)
   :printer #t
   :automaker #t)
 
+(defmethod (hot? (e Element))
+  (equal? (attr e) 'fire))
+
 ; (make Element)
 ; (make Element :attr 'fire)
-; (make-Element 'fire)
+; (make-Element "Calcifer" 'fire)
 (define elem (make Element))
 (set! (attr elem) 'wind)
+; elem
+; (who-are-you? elem)
 
 (defclass Animal (Thing)
+  (name :initvalue "an Animal")
   (gender :initvalue 'male)
   (size  :initvalue 'small)
-  (color :initvalue 'brown)
   :autoaccessors :slot
   :automaker #t
   :printer #t)
 
 ; (make-Animal)
 ; (make-Animal 'female)
-; (make-Animal 'female 'normal 'foo)
+; (make-Animal "Kittycat" 'female 'small)
 
 (defclass Pokemon (Animal Element)
+  (name :initvalue "a Pokemon") ; first without
   (index :initvalue 0
          :type <number>)
   :autoaccessors :slot
@@ -39,7 +55,12 @@
   :printer #t)
 
 (define p1 (make-Pokemon))
-(define p2 (make-Pokemon 'fire 'red 'female 'large 42))
+(define p2 (make-Pokemon "Charmander" 'fire 'female 'large 4))
+; p1
+; p2
+; (hot? p1)
+; (hot? p2)
+; (who-are-you? p1)
 
 (defgeneric attack ((t Thing))
   :combination generic-list-combination)
@@ -56,19 +77,19 @@
 (defclass Trainer ())
 
 (defmethod daily-routine ((t Trainer))
-  (println "He caught some Pokemon."))
+  (display "He caught some Pokemon."))
 (defmethod daily-routine :before ((t Trainer))
-  (println "He walked out."))
+  (display "He walked out."))
 (defmethod daily-routine :after ((t Trainer))
-  (println "He walked back home."))
+  (display "He walked back home."))
 
 (defclass Earlybird (Trainer))
 
 (defmethod daily-routine ((e Earlybird))
-  (println "He found two bird Pokemon in the morning."))
+  (display "He found two bird Pokemon in the morning."))
 (defmethod daily-routine :before ((e Earlybird))
-  (println "The sun just started rising."))
+  (display "The sun just started rising."))
 (defmethod daily-routine :after ((e Earlybird))
-  (println "There was still time before dinner."))
+  (display "There was still time before dinner."))
 
 ; (daily-routine (make Earlybird))
--- code/object-racket-example.rkt
+++ code/object-racket-example.rkt
@@ -2,28 +2,33 @@
 
 ; ------------ Klassen und Objekte ---------------
 (define Thing (class object% (super-new)
-                (init-field [i-am-a " Thing"])
-                (define/public (what-am-i?) 
-                  (string-append "I am a" i-am-a "!"))))
+                (init-field [name "a Thing"])
+                (define/public (who-are-you?) 
+                  (string-append "I am " name "!"))))
 
 ; (new Thing)
-; (send (new Thing what-am-i?))
+; (send (new Thing who-are-you?))
+; (send (make-object Thing "Bob") who-are-you?)
 
-(define Element (class Thing (super-new [i-am-a "n Element"])
+(define Element (class Thing
+                  (init [name "an Element"])
+                  (super-new [name name])
                   (init-field [attr 'water])
                   (define/public (hot?) (equal? attr 'fire))))
 
 (define elem (new Element))
-; (send elem what-am-i?)
+; (send elem who-are-you?)
 ; (get-field attr elem)
 ; (set-field! elem attr 'wind)
 ; (send elem hot?)
 
-(define Animal (class Thing (super-new [i-am-a "n Animal"])
+(define Animal (class Thing
+                 (init [name "an Animal"])
+                 (super-new [name name])
                  (init-field [gender 'male]
                              [size 'small])))
 
-; (send (new Animal) what-am-i?)
+; (send (new Animal) who-are-you?)
 
 
 ; ------------------ Mixins ---------------------
@@ -32,32 +37,44 @@
   (class superclass (super-new)))
 
 (define (Element-mixin %)
-  (class % (super-new) ; ! (super-new [i-am-a "n Element"])
+  (class % ;(super-new)
+    (init [name "an Element"])
+    (super-new [name name])
     (init-field [attr 'water])
-;    (define/public (attack) attr) ; !
+    ; ! (define/public (attack) attr)
     (define/override (attack) (list (super attack) attr))
     ))
 
 (define (Animal-mixin %)
-  (class % (super-new) ; ! (super-new [i-am-a "n Animal"])
+  (class % ; (super-new)
+    (init [name "an Animal"])
+    (super-new [name name])
     (init-field [gender 'male]
                 [size 'small])
     (define/public (attack) size)))
 
 (define Element2 (Element-mixin
-                  (class object% (super-new)
+                  (class Thing (super-new)
                     (define/public (attack) null))))
 ; (get-field attr (new Element2))
+; (send (new Element2) who-are-you?)
+; (send (new Element2 [name "Fire"]) who-are-you?)
 
 (define Animal2 (Animal-mixin Thing))
 ; (get-field size (new Animal [size 'huge] [gender 'female]))
+; (send (new Animal2) who-are-you?)
+; (send (new Animal2 [name "Cat"]) who-are-you?)
 
-(define Pokemon (Element-mixin (Animal-mixin Thing)))
+(define Pokemon (class (Element-mixin (Animal-mixin Thing))
+                  (init [name "a Pokemon"])
+                  (super-new [name name])))
 ; (define Pokemon (Animal-mixin (Element-mixin Thing))) ; !
 
 ; (get-field size (new Pokemon))
 ; (get-field attr (new Pokemon))
 ; (get-field gender (new Pokemon))
+; (send (new Pokemon) who-are-you?)
+; (send (new Pokemon [name "Charmander"]) who-are-you?)
 ; (send (new Pokemon) attack)
 (define p (new Pokemon [size 'large]
                        [gender 'female]
@@ -102,15 +119,24 @@
 
 (define Pokemon-mixin (trait->mixin Pokemon-trait))
 
-(define Pokemon2 (Pokemon-mixin
-                  (class Thing (super-new [i-am-a " Pokemon"])
-                    (init-rest args))))
+(define Pokemon2 (class (Pokemon-mixin Thing)
+                   (init [this-name "a Pokemon"]
+                         [this-size 'small]
+                         [this-gender 'male]
+                         [this-attr 'water])
+                   (super-new [name this-name])
+                   (inherit-field size gender attr)
+                   (set! size this-size)
+                   (set! gender this-gender)
+                   (set! attr this-attr)))
 
 (define p2 (new Pokemon2))
-; (send p2 what-am-i?)
-; (send p2 attack)
-(define p3 (make-object Pokemon2 'large 'female 'fire))
-; (send p3 attack)
+(send p2 who-are-you?)
+(send p2 attack)
+
+(define p3 (make-object Pokemon2 "Charmander" 'large 'female 'fire))
+(send p3 who-are-you?)
+(send p3 attack)
 
 ; ------------ Ergänzungsmethoden ---------------
 
