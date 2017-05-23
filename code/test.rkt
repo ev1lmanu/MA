e25f5a6bb0eb129d5ac6756587a1f86702610a26
Manuela Beckert <9beckert@informatik.uni-hamburg.de>
2017-05-23 23:13:49 +0200

    donegit status

--- code/test.rkt
+++ code/test.rkt
@@ -1,40 +1,56 @@
 #lang racket
 (require "classmacro.rkt")
 
-(define Thing (class () (super-new)
+(define Thing (class object% (super-new)
                 (init-field [name "a Thing"])
                 (define/public (who-are-you?)
                   (string-append "I am " name "!"))
-                ; Definition einer generische Funktion
+                ; generic function
                 (define/generic (attack)
-                  ; Es kann eine beliebige Funktion angegeben werden,
-                  ; die sich auf eine Liste apply-en lässt
-                  (compose reverse list))
-                (define/generic (x) list)))
+                  (compose reverse list))))
+
+;> (send (new Thing) who-are-you?)
+;> (send (new Thing [name "Bob"]) who-are-you?)
 
 (define Element (class Thing 
                   (init [name "an Element"])
                   (super-new [name name])
                   (init-field [attr 'water])
                   (define/public (hot?) (equal? attr 'fire))
+                  (define/override (who-are-you?)
+                    (string-append (super who-are-you?)
+                                   (if (hot?) " And I am hot!" "")))
+                  ; implementing method
                   (define/public (attack) attr)))
+
+;> (send (new Element) who-are-you?)
+;> (send (new Element [attr 'fire]) who-are-you?)
+;> (send (new Element) attack)
 
 (define Animal (class Thing
                  (init [name "an Animal"])
                  (super-new [name name])
                  (init-field [size 'small])
+                 ; implementing method
                  (define/public (attack) size)))
 
-; Mehrfachvererbung durch Angabe einer (ungequoteten) Liste von Superklassen
+;> (send (new Animal) who-are-you?)
+;> (send (new Animal [name "Bob"]) who-are-you?)
+;> (send (new Animal) attack)
+
+; multiple inheritance by supplying an (unquoted) super class list
 (define Pokemon (class (Element Animal)
-                  (inherit-field attr size)
                   (init [name "a Pokemon"])
                   (super-new [name name])
-                  (define/public (attack) 'ball)))
+                  ; implementing method
+                  (define/public (attack) 'ball)
+                  ; inherit fields that are needed for combination
+                  (inherit-field attr size)))
 
 ; Beispiel für die Methodenkombination
 (send (new Pokemon) who-are-you?)
 (send (new Pokemon) hot?)
-(send (new Pokemon [attr 'fire] [size 'big]) hot?)
+(get-field size (new Pokemon [name "Bob"] [attr 'fire] [size 'big]))
+(send (new Pokemon [attr 'fire] [size 'big]) who-are-you?)
 (send (new Pokemon) attack)
 (send (new Pokemon [attr 'fire] [size 'big]) attack)
